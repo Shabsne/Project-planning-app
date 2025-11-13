27e958053f3ca62f57789bac023f44f9c60eefac
NikolajPedersen29 <mejlby2002@hotmail.com>
2025-11-13 10:06:41 +0100

    Started project

---
+++ src/main/java/org/example/projectplanningapp/ProjectPlanningAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectplanningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlanningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPlanningAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectplanningapp/ProjectPlanningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectplanningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlanningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
