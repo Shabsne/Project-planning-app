3f664923d26ca7dd454be86959f9620b2b496e80
Uller <fullersted@gmail.com>
2025-12-03 10:35:43 +0100

    Fixed rowmappers added methods and html pages

--- src/main/java/org/example/projectplanningapp/repositories/rowMappers/TaskRowMapper.java
+++ src/main/java/org/example/projectplanningapp/repositories/rowMappers/TaskRowMapper.java
@@ -1,7 +1,6 @@
 package org.example.projectplanningapp.repositories.rowMappers;
 
 
-import org.example.projectplanningapp.models.Employee;
 import org.example.projectplanningapp.models.Project;
 import org.example.projectplanningapp.models.Status;
 import org.example.projectplanningapp.models.Task;
@@ -10,25 +9,34 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class TaskRowMapper implements RowMapper<Task> {
 
     @Override
-    public Task mapRow (ResultSet rs,int rowNum) throws SQLException {
+    public Task mapRow(ResultSet rs, int rowNum) throws SQLException {
         Task task = new Task();
-        task.setId(rs.getInt("id"));
+        task.setId(rs.getInt("taskId"));
         task.setTitle(rs.getString("title"));
         task.setDescription(rs.getString("description"));
-        task.setStatus((Status) rs.getObject("status"));
+        task.setStatus(Status.valueOf(rs.getString("status")));
         task.setEstimatedHours(rs.getInt("estimatedHours"));
         task.setActualHours(rs.getInt("actualHours"));
-        task.setDeadline((LocalDateTime) rs.getObject("deadline"));
-        task.setParentProject((Project) rs.getObject("parentProject"));
-        task.setParentTask((Task) rs.getObject("parentTask"));
-        task.setSubTasks((List<Task>) rs.getObject("subTasks"));
-        task.setAssignedEmployees((List<Employee>) rs.getObject("assignedEmployees"));
+        task.setDeadline(rs.getTimestamp("deadline").toLocalDateTime());
+
+        // Parent project
+        int projectId = rs.getInt("parentProjectId");
+        task.setParentProject(new Project(projectId));
+
+        // Parent task (optional)
+        try {
+            int parentTaskId = rs.getInt("parentTaskId");
+            if (!rs.wasNull()) {
+                Task parentTask = new Task();
+                parentTask.setId(parentTaskId);
+                task.setParentTask(parentTask);
+            }
+        } catch (SQLException ignored) {}
+
         return task;
     }
 }
