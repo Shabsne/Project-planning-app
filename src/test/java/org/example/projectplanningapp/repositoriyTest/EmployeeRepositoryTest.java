3779887c424b1b71f5bcab796e193b730a14d519
NikolajPedersen29 <mejlby2002@hotmail.com>
2025-12-03 10:15:45 +0100

    Created tests

--- src/test/java/org/example/projectplanningapp/repositoriyTest/EmployeeRepositoryTest.java
+++ src/test/java/org/example/projectplanningapp/repositoriyTest/EmployeeRepositoryTest.java
@@ -2,31 +2,47 @@ package org.example.projectplanningapp.repositoriyTest;
 
 import org.example.projectplanningapp.models.Employee;
 import org.example.projectplanningapp.repositories.EmployeeRepository;
+import org.example.projectplanningapp.repositories.rowMappers.EmployeeRowMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EmployeeRepositoryTest {
 
-    @Autowired
-    private EmployeeRepository repository;
-
-    @Autowired
     private JdbcTemplate jdbcTemplate;
+    private EmployeeRepository employeeRepository;
+
 
     @BeforeEach
     void setup() {
-        jdbcTemplate.update("DELETE FROM Employee");
-        jdbcTemplate.update("INSERT INTO Employee (employeeRoleId, name, email, password) VALUES (1,'Test User','test@mail.com','1234')");
+        jdbcTemplate = mock(JdbcTemplate.class);
+        employeeRepository = new EmployeeRepository(jdbcTemplate);
     }
 
     @Test
     void findByEmail_returnsEmployee_whenEmailExists() {
-        Employee employee = repository.findByEmail("test@mail.com");
+        Employee expected = new Employee();
+        expected.setEmployeeId(1);
+        expected.setName("Test User");
+        expected.setEmail("test@mail.com");
+        expected.setPassword("1234");
 
+        when(jdbcTemplate.queryForObject(
+                anyString(),
+                any(EmployeeRowMapper.class),
+                eq("test@mail.com")
+        )).thenReturn(expected);
+
+        // Act
+        Employee employee = employeeRepository.findByEmail("test@mail.com");
+
+        // Assert
         assertNotNull(employee);
         assertEquals("Test User", employee.getName());
         assertEquals("test@mail.com", employee.getEmail());
@@ -34,7 +50,7 @@ public class EmployeeRepositoryTest {
 
     @Test
     void findByEmail_returnsNull_whenEmailDoesNotExist() {
-        Employee employee = repository.findByEmail("unknown@mail.com");
+        Employee employee = employeeRepository.findByEmail("unknown@mail.com");
 
         assertNull(employee);
     }
