2b8883fdbb9b5ffb915dddf240839c3502edad23
sebastianvij <sevi0001@stud.kea.dk>
2025-12-16 01:10:31 +0100

    Updated project findById method to throw custom ResourceNotFound exception if it returns null

--- src/main/java/org/example/projectplanningapp/services/ProjectService.java
+++ src/main/java/org/example/projectplanningapp/services/ProjectService.java
@@ -1,5 +1,6 @@
 package org.example.projectplanningapp.services;
 
+import org.example.projectplanningapp.exceptions.ResourceNotFoundException;
 import org.example.projectplanningapp.models.Employee;
 import org.example.projectplanningapp.models.Project;
 import org.example.projectplanningapp.models.Status;
@@ -32,7 +33,13 @@ public class ProjectService {
     }
 
     public Project findById(int id) {
-        return projectRepository.findProjectById(id);
+        Project project = projectRepository.findProjectById(id);
+
+        if (project == null) {
+            throw new ResourceNotFoundException("Projekt", id);
+        }
+
+        return project;
     }
 
     public void updateProject(Project project) {
